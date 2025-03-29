c763b51a3c6d6feb8d0c8474749cd4a1fba96562
Aleksey <mr.ryabov.aa@yandex.ru>
2025-03-29 09:31:24 +0300

    Add files project

---
+++ src/test/java/ru/ryaboman/projects/bankAccount/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ryaboman.projects.bankAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
